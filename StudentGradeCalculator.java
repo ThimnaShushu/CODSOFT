912d7c0c586535338d0c89430bb1c8cff9355ac5
ThimnaShushu <thimnashushu@gmail.com>
2024-06-27 22:44:27 +0200

    The scanner class is in use to handle input and the input pf user is handled

--- StudentGradeCalculator.java
+++ StudentGradeCalculator.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class StudentGradeCalculator {
     public static void main(String[] args) {
+        Scanner s1 = new Scanner(System.in);
+        System.out.println("Enter the total number of subjects: ");
+        int len = s1.nextInt();
+        System.out.println("Please enter a list of your subject totals (7 subjects)");
         
     }
 }
