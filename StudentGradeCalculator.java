0cf664846d4dea5d840b2c50e5c20d866c3aea78
ThimnaShushu <thimnashushu@gmail.com>
2024-06-27 23:21:34 +0200

    The list of marks are able to be added to output a total of marks

--- StudentGradeCalculator.java
+++ StudentGradeCalculator.java
@@ -5,17 +5,27 @@ public class StudentGradeCalculator {
         Scanner s1 = new Scanner(System.in);
         System.out.println("Enter the total number of subjects: ");
         int len = s1.nextInt();
+        s1.nextLine();
         System.out.println("Please enter a list of your subject totals: ");
         String inputted = s1.nextLine();
         String[] inputList = inputted.split(",");
 
         double[] marksList = new double[len];
 
-        for(i=0; i<inputList.length; i++){
+        try{
+        for(int i=0; i<inputList.length; i++){
             marksList[i] = Double.parseDouble(inputList[i]);
         }
-
-        
+        }
+        catch (NumberFormatException e){
+            System.out.println("The input string is invalid");
+        }
+        //Total Marks Calculations:
+        Double sum = 0.0;
+        for (int j = 0; j<marksList.length; j++){
+            sum = sum + marksList[j];
+        }
+        System.out.println("Total Marks: "+ sum);
         
 
         
