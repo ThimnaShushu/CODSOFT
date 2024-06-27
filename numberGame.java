a0b24bf848c65d9b33b9611e212db3bba5e38159
ThimnaShushu <thimnashushu@gmail.com>
2024-06-28 00:01:42 +0200

    The number game class has been created, where the random number max is specifieddddddd

---
+++ numberGame.java
@@ -0,0 +1,12 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class numberGame {
+    public static void main(String[] args) {
+    Scanner s1 = new Scanner(System.in);
+    Random randomNum = new Random(101);
+
+    
+
+    }
+}
