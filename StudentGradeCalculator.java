bfd0764d0ff476abfead6bd6665c8770a3e72c23
ThimnaShushu <thimnashushu@gmail.com>
2024-06-27 23:04:00 +0200

    The input is taken to become a lit which is then going to be manipulated

--- StudentGradeCalculator.java
+++ StudentGradeCalculator.java
@@ -5,7 +5,19 @@ public class StudentGradeCalculator {
         Scanner s1 = new Scanner(System.in);
         System.out.println("Enter the total number of subjects: ");
         int len = s1.nextInt();
-        System.out.println("Please enter a list of your subject totals (7 subjects)");
+        System.out.println("Please enter a list of your subject totals: ");
+        String inputted = s1.nextLine();
+        String[] inputList = inputted.split(",");
+
+        double[] marksList = new double[len];
+
+        for(i=0; i<inputList.length; i++){
+            marksList[i] = Double.parseDouble(inputList[i]);
+        }
+
+        
+        
+
         
     }
 }
