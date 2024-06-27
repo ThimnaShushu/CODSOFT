3061c791329c8d126ff55aac9ec04c97664c869c
ThimnaShushu <thimnashushu@gmail.com>
2024-06-28 00:20:14 +0200

    The number game class is able to generate random number and compare guess to random number

--- numberGame.java
+++ numberGame.java
@@ -4,8 +4,28 @@ import java.util.Scanner;
 public class numberGame {
     public static void main(String[] args) {
     Scanner s1 = new Scanner(System.in);
-    Random randomNum = new Random(101);
+    Random random = new Random();
+    int randomNum = random.nextInt(101);
 
+    while(true){
+        System.out.println("Enter a number as a guess: (Between 1-100)");
+        int guessNum = s1.nextInt();
+
+        if(guessNum == randomNum){
+            System.out.println("Correct! You have guessed the correct number!");
+
+        }
+        else{
+        if(guessNum < randomNum){
+            System.out.println("Guess is too low, Try guessing higher.");
+        }
+        else if(guessNum > randomNum){
+            System.out.println("Guess is too high, Try guessing Higher.");
+        }
+    }
+
+        
+    }
     
 
     }
